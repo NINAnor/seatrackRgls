e2f980d0d7e585195a8f5ae9b5fbb949144977dd
Vegard Sandøy Bråthen <vegard.brathen@nina.no>
2025-11-06 14:22:28 +0100

    description added to 'aut_midnight_sun_removal'

--- R/FUNCTION_aut_midnight_sun_removal.R
+++ R/FUNCTION_aut_midnight_sun_removal.R
@@ -1,3 +1,11 @@
+#' Remove false positions during breeding or midnight sun periods
+#'
+#' At the head or tail of a yearly track (e.g breedingY to breedingY+1) : removes the first or last positions that are more than 25 hours apart in time from the other positions.
+#' Datagaps of >25 hours in the midst of the track won't be assessed or filtered.
+#'
+#' @param df data.frame where  'tFirst' and 'tSecond' (both date_time) must be part of the first eight columns
+#' @return A data.frame where non-consequative positions have been removed from the head and tail of the original data.frame
+#' @export
 aut_midnight_sun_removal <- function(df) {
   df$conf <- 9
   tail_tmp <- df
